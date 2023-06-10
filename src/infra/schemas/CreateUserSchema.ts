b5b4c72baa115554518a9ef27996b41e425199ae
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-10 17:00:15 -0300

    email validation on create and find user schemas

--- src/infra/schemas/CreateUserSchema.ts
+++ src/infra/schemas/CreateUserSchema.ts
@@ -1,6 +1,10 @@
 export default {
   validate(payload: any) {
-    if (!payload.email) throw new Error('"email" is required')
+    const { email } = payload
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
+
+    if (!email) throw new Error('"email" is required')
+    if (!emailRegex.test(email)) throw new Error('Invalid "email" format')
     if (!payload.password) throw new Error('"password" is required')
     if (!payload.confirm_password) throw new Error('"confirm_password" is required')
   }
--- src/infra/schemas/FindUsersSchema.ts
+++ src/infra/schemas/FindUsersSchema.ts
@@ -4,9 +4,11 @@ export default {
       switch (key) {
         case 'email':
           const { email } = payload
+          const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
 
           if (!email) throw new Error('"email" is empty')
           if (typeof email !== 'string') throw new Error('"email" must be a string')
+          if (!emailRegex.test(email)) throw new Error('Invalid "email" format')
 
           break
       
