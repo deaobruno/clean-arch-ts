fe35ce95170a165ce6e197ca6f2c202dace62ea6
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-18 23:07:24 -0300

    test: updated create user schema test

--- test/unit/infra/schemas/CreateUserSchema.test.ts
+++ test/unit/infra/schemas/CreateUserSchema.test.ts
@@ -1,12 +1,13 @@
+import { faker } from '@faker-js/faker'
 import { expect } from 'chai'
-import CreateUserSchema from '../../../../src/infra/schemas/CreateUserSchema'
+import CreateUserSchema from '../../../../src/infra/schemas/user/CreateUserSchema'
 
 const { validate } = CreateUserSchema
 
 describe('/infra/schemas/CreateUserSchema.ts', () => {
   it('should execute without errors', () => {
     validate({
-      email: 'test',
+      email: faker.internet.email(),
       password: 'test',
       confirm_password: 'test',
     })
@@ -15,29 +16,52 @@ describe('/infra/schemas/CreateUserSchema.ts', () => {
   })
 
   it('should fail when missing email', () => {
-    expect(() => validate({
+    const validation = <Error>validate({
       email: '',
       password: 'test',
       confirm_password: 'test',
-    }))
-      .throw('"email" is required')
+    })
+
+    expect(validation.message).equal('"email" is required')
+  })
+
+  it('should fail when passing invalid email', () => {
+    const validation = <Error>validate({
+      email: 'test',
+      password: 'test',
+      confirm_password: 'test',
+    })
+
+    expect(validation.message).equal('Invalid "email" format')
   })
 
   it('should fail when missing password', () => {
-    expect(() => validate({
-      email: 'test',
+    const validation = <Error>validate({
+      email: faker.internet.email(),
       password: '',
       confirm_password: 'test',
-    }))
-      .throw('"password" is required')
+    })
+
+    expect(validation.message).equal('"password" is required')
   })
 
   it('should fail when missing confirm_password', () => {
-    expect(() => validate({
-      email: 'test',
+    const validation = <Error>validate({
+      email: faker.internet.email(),
       password: 'test',
       confirm_password: '',
-    }))
-      .throw('"confirm_password" is required')
+    })
+
+    expect(validation.message).equal('"confirm_password" is required')
+  })
+
+  it('should fail when password and confirm_password are different', () => {
+    const validation = <Error>validate({
+      email: faker.internet.email(),
+      password: 'test',
+      confirm_password: 'tes',
+    })
+
+    expect(validation.message).equal('Passwords mismatch')
   })
 })
