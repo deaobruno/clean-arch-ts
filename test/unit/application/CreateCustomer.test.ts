327f4ada459a7e766ad5b44e7fda9a2761384782
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-18 21:25:19 -0300

    test: updated create customer use case test

--- test/unit/application/CreateCustomer.test.ts
+++ test/unit/application/CreateCustomer.test.ts
@@ -1,14 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { expect } from 'chai'
-import CreateCustomer from '../../../src/application/user/CreateCustomer'
-import { User } from '../../../src/domain/User'
-import IUserRepository from '../../../src/domain/repositories/UserRepository'
+import CreateCustomer from '../../../src/application/use_cases/user/CreateCustomer'
+import { LevelEnum, User } from '../../../src/domain/User'
+import IUserRepository from '../../../src/domain/repositories/IUserRepository'
+import CryptoDriver from '../../../src/infra/drivers/CryptoDriver'
 
 const userRepository: IUserRepository = {
-  async save(data: any): Promise<User> {
+  async save(data: any, filters?: object): Promise<User> {
     return data
   },
-  async find(params?: any): Promise<User[]> {
+  async find(filters?: any): Promise<User[]> {
     return []
   },
   async findCustomers(): Promise<User[]> {
@@ -17,72 +18,60 @@ const userRepository: IUserRepository = {
   async findAdmins(): Promise<User[]> {
     return []
   },
-  async findOne(params: any): Promise<User | undefined> {
-    return
-  },
-  async findOneById(id: string): Promise<User | undefined> {
+  async findOne(filters: any): Promise<User | undefined> {
     return
   },
   async findOneByEmail(email: string): Promise<User | undefined> {
     return
   },
-  async exists(): Promise<boolean> {
-    return false
-  },
-  async delete(id: string): Promise<void> {
-    return
-  },
-  async deleteMany(filters?: any): Promise<void> {
+  async delete(filters: any): Promise<void> {
     return
   },
 }
 
+const cryptoDriver: CryptoDriver = {
+  generateID: () => faker.datatype.uuid()
+}
+
+const user_id = faker.datatype.uuid()
 const email = faker.internet.email()
 const password = '$Baw@Y;nH5dUq!HRBkGctpiFLYM=73icS^_?e#dTWmM[?g]:1aV#X&w]bXp:KC+h'
 
 describe('/application/CreateCustomer.ts', () => {
-  const createCustomerUseCase = new CreateCustomer(userRepository)
+  const createCustomerUseCase = new CreateCustomer(userRepository, cryptoDriver)
 
-  it('should successfully create an User', async () => {
+  it('should successfully create a Customer', async () => {
     const userParams = {
       email,
       password,
       confirm_password: password,
-      level: 0
+      level: 2
     }
 
-    await createCustomerUseCase.exec(userParams)
+    const user = await createCustomerUseCase.exec(userParams)
+
+    expect(typeof user.user_id).equal('string')
+    expect(user.email).equal(userParams.email)
+    expect(user.password).equal(userParams.password)
+    expect(user.level).equal(LevelEnum.CUSTOMER)
+    expect(user.isCustomer).equal(true)
   })
 
-  it('should fail when trying to create an User with repeated email', async () => {
+  it('should fail when trying to create a Customer with repeated email', async () => {
     const userParams = {
-      email: email,
+      email,
       password,
       confirm_password: password,
       level: 0
     }
 
     userRepository.findOneByEmail = async (email) => {
-      return User.create(userParams)
+      return User.create({ user_id, ...userParams })
     }
 
     await createCustomerUseCase.exec(userParams)
       .catch((error) => {
         expect(error.message).equal('Email already in use')
-      })
-  })
-
-  it('should fail when trying to create an User with mismatching passwords', async () => {
-    const userParams = {
-      email,
-      password,
-      confirm_password: 'password',
-      level: 0
-    }
-
-    await createCustomerUseCase.exec(userParams)
-      .catch((error) => {
-        expect(error.message).equal('Passwords mismatching')
       })
   })
 })
