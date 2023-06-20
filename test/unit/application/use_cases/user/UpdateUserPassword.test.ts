2bcfbdde25a6d57117635336ea316da30dc9291b
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-19 23:02:15 -0300

    test: update user password use case

--- test/unit/application/use_cases/user/UpdateUserPassword.test.ts
+++ test/unit/application/use_cases/user/UpdateUserPassword.test.ts
@@ -3,11 +3,11 @@ import { expect } from "chai"
 import InMemoryDriver from "../../../../../src/infra/drivers/InMemoryDriver"
 import UserRepository from "../../../../../src/adapters/repositories/UserRepository"
 import { LevelEnum, User } from "../../../../../src/domain/User"
-import UpdateUser from '../../../../../src/application/use_cases/user/UpdateUser'
+import UpdateUserPassword from '../../../../../src/application/use_cases/user/UpdateUserPassword'
 
 const inMemoryDriver = new InMemoryDriver()
 const userRepository = new UserRepository(inMemoryDriver)
-const updateUser = new UpdateUser(userRepository)
+const updateUserPassword = new UpdateUserPassword(userRepository)
 const user_id = faker.datatype.uuid()
 const userData = {
   user_id,
@@ -16,25 +16,24 @@ const userData = {
   level: LevelEnum.CUSTOMER
 }
 
-describe('/application/use_cases/user/UpdateUser.ts', () => {
+describe('/application/use_cases/user/UpdateUserPassword.ts', () => {
   before(async () => await userRepository.save(User.create(userData)))
 
-  it('should update an existing user', async () => {
-    const newEmail = faker.internet.email()
+  it('should update the password of an existing user', async () => {
     const updateData = {
       userId: user_id,
-      email: newEmail,
+      password: faker.internet.password(),
     }
-    const result = await updateUser.exec(updateData)
+    const result = await updateUserPassword.exec(updateData)
 
-    expect(result.user_id).equal(user_id)
-    expect(result.email).equal(newEmail)
-    expect(result.password).equal(userData.password)
+    expect(result.user_id).equal(userData.user_id)
+    expect(result.email).equal(userData.email)
+    expect(result.password).not.equal(userData.password)
     expect(result.level).equal(userData.level)
   })
 
-  it('should fail when trying to update an user passing wrong ID', async () => {
-    await updateUser.exec({ userId: '' })
+  it('should fail when trying to update an user password passing wrong ID', async () => {
+    await updateUserPassword.exec({ userId: '', password: '' })
       .catch(error => {
         expect(error.message).equal('User not found')
       })
