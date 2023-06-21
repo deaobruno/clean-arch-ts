455563b3242ed3427b42188cf0d56f0d447a4a2c
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-20 23:49:05 -0300

    fix: fixed injected db driver type in user repo

--- src/adapters/repositories/UserRepository.ts
+++ src/adapters/repositories/UserRepository.ts
@@ -1,27 +1,27 @@
 import { User } from '../../domain/User'
+import IRepository from '../../domain/repositories/IRepository'
 import IUserRepository from '../../domain/repositories/IUserRepository'
-import InMemoryDriver from '../../infra/drivers/InMemoryDriver'
 
 export default class UserRepository implements IUserRepository {
-  constructor(private _driver: InMemoryDriver) {}
+  constructor(private _dbDriver: IRepository<User>) {}
 
-  find = (filters?: object) => (this._driver.find)(filters)
-  findOne = (filters: object) => (this._driver.findOne)(filters)
-  delete = (filters: object) => (this._driver.delete)(filters)
+  find = (filters?: object) => (this._dbDriver.find)(filters)
+  findOne = (filters: object) => (this._dbDriver.findOne)(filters)
+  delete = (filters: object) => (this._dbDriver.delete)(filters)
 
   async save(entity: User) {
-    return this._driver.save(entity, { user_id: entity.user_id })
+    return this._dbDriver.save(entity, { user_id: entity.user_id })
   }
 
   async findOneByEmail(email: string): Promise<User | undefined> {
-    return this._driver.findOne({ email })
+    return this._dbDriver.findOne({ email })
   }
 
   async findAdmins(): Promise<User[]> {
-    return this._driver.find({ level: 1 })
+    return this._dbDriver.find({ level: 1 })
   }
 
   async findCustomers(): Promise<User[]> {
-    return this._driver.find({ level: 2 })
+    return this._dbDriver.find({ level: 2 })
   }
 }
