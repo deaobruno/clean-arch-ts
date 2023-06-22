9507418be75d486c0872718485806485d495e583
Bruno Oliveira <bruno.deao@gmail.com>
2023-06-21 23:44:33 -0300

    test: updated CreateAdminSchema.test.ts

--- test/unit/infra/schemas/user/CreateAdminSchema.test.ts
+++ test/unit/infra/schemas/user/CreateAdminSchema.test.ts
@@ -3,11 +3,12 @@ import { expect } from 'chai'
 import CreateAdminSchema from '../../../../../src/infra/schemas/user/CreateAdminSchema'
 
 const { validate } = CreateAdminSchema
+const email = faker.internet.email()
 
 describe('/infra/schemas/user/CreateAdminSchema.ts', () => {
   it('should execute without errors', () => {
     const validation = validate({
-      email: faker.internet.email(),
+      email,
       password: 'test',
       confirm_password: 'test',
     })
@@ -25,6 +26,46 @@ describe('/infra/schemas/user/CreateAdminSchema.ts', () => {
     expect(validation.message).equal('"email" is required')
   })
 
+  it('should fail when passing a number as email', () => {
+    const validation = <Error>validate({
+      email: 123,
+      password: 'test',
+      confirm_password: 'test',
+    })
+
+    expect(validation.message).equal('Invalid "email" format')
+  })
+
+  it('should fail when passing a boolean as email', () => {
+    const validation = <Error>validate({
+      email: true,
+      password: 'test',
+      confirm_password: 'test',
+    })
+
+    expect(validation.message).equal('Invalid "email" format')
+  })
+
+  it('should fail when passing an object as email', () => {
+    const validation = <Error>validate({
+      email: { test: 'test' },
+      password: 'test',
+      confirm_password: 'test',
+    })
+
+    expect(validation.message).equal('Invalid "email" format')
+  })
+
+  it('should fail when passing a Bigint as email', () => {
+    const validation = <Error>validate({
+      email: BigInt(9007199254740991n),
+      password: 'test',
+      confirm_password: 'test',
+    })
+
+    expect(validation.message).equal('Invalid "email" format')
+  })
+
   it('should fail when passing invalid email', () => {
     const validation = <Error>validate({
       email: 'test',
@@ -37,7 +78,7 @@ describe('/infra/schemas/user/CreateAdminSchema.ts', () => {
 
   it('should fail when missing password', () => {
     const validation = <Error>validate({
-      email: faker.internet.email(),
+      email,
       password: '',
       confirm_password: 'test',
     })
@@ -47,7 +88,7 @@ describe('/infra/schemas/user/CreateAdminSchema.ts', () => {
 
   it('should fail when missing confirm_password', () => {
     const validation = <Error>validate({
-      email: faker.internet.email(),
+      email,
       password: 'test',
       confirm_password: '',
     })
@@ -57,11 +98,22 @@ describe('/infra/schemas/user/CreateAdminSchema.ts', () => {
 
   it('should fail when password and confirm_password are different', () => {
     const validation = <Error>validate({
-      email: faker.internet.email(),
+      email,
       password: 'test',
       confirm_password: 'tes',
     })
 
     expect(validation.message).equal('Passwords mismatch')
+  })
+
+  it('should fail when passing invalid param', () => {
+    const validation = <Error>validate({
+      email,
+      password: 'test',
+      confirm_password: 'test',
+      test: 'test'
+    })
+
+    expect(validation.message).equal('Invalid param: "test"')
   })
 })
