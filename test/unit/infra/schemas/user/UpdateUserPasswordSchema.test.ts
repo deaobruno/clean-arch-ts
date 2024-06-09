877b3dcf7ebdbcf9dba1dd92207d1df14eba2173
Bruno Oliveira <bruno.deao@gmail.com>
2024-06-09 17:30:37 -0300

    updated password validation in update password endpoint

--- test/unit/infra/schemas/user/UpdateUserPasswordSchema.test.ts
+++ test/unit/infra/schemas/user/UpdateUserPasswordSchema.test.ts
@@ -4,7 +4,7 @@ import UpdateUserPasswordSchema from '../../../../../src/infra/schemas/user/Upda
 
 const { validate } = UpdateUserPasswordSchema;
 const user_id = faker.string.uuid();
-const password = faker.internet.password();
+const password = `${faker.string.alpha({ length: 2, casing: 'lower' })}${faker.string.alpha({ length: 2, casing: 'upper' })}${faker.string.numeric({ length: 2 })}${faker.string.fromCharacters('!@#$&*', 2)}`;
 
 describe('/infra/schemas/user/UpdateUserPasswordSchema.ts', () => {
   it('should execute without errors', () => {
@@ -85,6 +85,82 @@ describe('/infra/schemas/user/UpdateUserPasswordSchema.ts', () => {
     });
 
     expect(validation.message).equal('"password" is not allowed to be empty');
+  });
+
+  it('should fail when password length is lower than 8', () => {
+    const validation = <Error>validate({
+      user_id,
+      password: `${faker.string.alpha(7)}`,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      '"password" length must be at least 8 characters long',
+    );
+  });
+
+  it('should fail when password length is greater than 64', () => {
+    const validation = <Error>validate({
+      user_id,
+      password: `${faker.string.alpha(65)}`,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      '"password" length must be less than or equal to 64 characters long',
+    );
+  });
+
+  it('should fail when password does not have a lower case character', () => {
+    const wrongPassword = `${faker.string.alpha({ length: 4, casing: 'upper' })}${faker.string.numeric({ length: 2 })}${faker.string.fromCharacters('!@#$&*', 2)}`;
+    const validation = <Error>validate({
+      user_id,
+      password: wrongPassword,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      `"password" with value "${wrongPassword}" fails to match the required pattern: /.*[a-z]/`,
+    );
+  });
+
+  it('should fail when password does not have an upper case character', () => {
+    const wrongPassword = `${faker.string.alpha({ length: 4, casing: 'lower' })}${faker.string.numeric({ length: 2 })}${faker.string.fromCharacters('!@#$&*', 2)}`;
+    const validation = <Error>validate({
+      user_id,
+      password: wrongPassword,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      `"password" with value "${wrongPassword}" fails to match the required pattern: /.*[A-Z]/`,
+    );
+  });
+
+  it('should fail when password does not have a number character', () => {
+    const wrongPassword = `${faker.string.alpha({ length: 2, casing: 'lower' })}${faker.string.alpha({ length: 2, casing: 'upper' })}${faker.string.fromCharacters('!@#$&*', 4)}`;
+    const validation = <Error>validate({
+      user_id,
+      password: wrongPassword,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      `"password" with value "${wrongPassword}" fails to match the required pattern: /.*[0-9]/`,
+    );
+  });
+
+  it('should fail when password does not have a special character', () => {
+    const wrongPassword = `${faker.string.alpha({ length: 2, casing: 'lower' })}${faker.string.alpha({ length: 2, casing: 'upper' })}${faker.string.numeric({ length: 4 })}`;
+    const validation = <Error>validate({
+      user_id,
+      password: wrongPassword,
+      confirm_password: password,
+    });
+
+    expect(validation.message).equal(
+      `"password" with value "${wrongPassword}" fails to match the required pattern: /.*[!@#$&*]/`,
+    );
   });
 
   it('should fail when confirm_password is empty', () => {
